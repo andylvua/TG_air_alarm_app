3646af7fc1e29ea9e29a06cdb6f9f21c2fe723c0
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-03-01 13:10:19 +0200

    Fixed

--- json_formatter.py
+++ json_formatter.py
@@ -1,5 +1,5 @@
-def get_message(parsed_json):
-    for i in range(len(parsed_json)-1):
+def get_last_message(parsed_json):
+    for i in range(len(parsed_json)):
         if parsed_json[i]["_"] == "Message":
             message = parsed_json[i]["message"]
-            return str(i+1) + " Message: " + str(message)
+            return "Last message: " + str(message)
